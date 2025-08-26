41e2f50ca6da3693c1e9804c2a034d58fa8af9b4
Mitchell Lagaras <mookielagaras@gmail.com>
2025-08-26 05:07:24 -0400

    Moved hardcoded dLcav figures to ohio System variables

--- submarine_detection.py
+++ submarine_detection.py
@@ -8,26 +8,23 @@ v0 is the knots after which cavitation will increase audibility
 n is the noise scaling component
 '''
 
-ohio = System(L0=100, v0=21, n=2.5)
+ohio = System(L0=100, v0=21, n=2.5, p = 2.5, A = 0.3125)
 
 L0 = 100 # estimated 'resting' noise level of Ohio class sub (or similar) before audibility of cavitation in dB
 v0 = 21 # knots after which cavitation will increase audibility (i.e. maximum cruise speed at L0db
 n = 2.5 # noise scaling component. Assumed to be between 2 and 3 given an estimated max noise output of 110
 
-def dLcav(v): 
+def dLcav(v, system): 
   '''
   Solving A * (v - 25)** p where A is the cavitation scaling factor and p is the cavitation noise growth rate.
-  Based on reasonable trial and error.
-  Given the estimated max speed of a submerged Ohio (25 knots) we can solve for A:
 
   A = 10 / (4**p)
   Smaller values of p lead to more linear increases which are not necessarily accurate given dB are logarithmic.
-  p must be higher, around 2.5 made the most sense given estimated max speed of sub.
   '''
-  if v <= v0: # Below cavitation threshold so doesn't matter--0
+  if v <= system.v0: # Below cavitation threshold
     return 0
   else:
-    return 0.3125 * (v - v0)**2.5
+    return system.A * (v - system.v0)**system.p
 
 def get_noise_level(v, system):
   x = 1 + (v / system.v0)**system.n
@@ -36,7 +33,8 @@ def get_noise_level(v, system):
 def get_transmission_loss(r): 
   '''
   r = distance
-  alpha = Rate at which dB dissipates as it travels through a medium (0.04 dB/km is a good rate for seawater)
+  alpha = Rate at which dB dissipates as it travels through a medium (0.04 dB/km is a good rate for seawater).
+  Using 20 to model wide open seas. Shallower waters will require 10 (cylindrical)
   '''
   alpha = 0.00004
   return 20 * (np.log(r) / np.log(10)) + alpha * r
